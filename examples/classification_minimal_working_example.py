00a60315cee97ee2900c7b31a24d1bf9cc91f215
Curtis G. Northcutt <curtis.northcutt@gmail.com>
2018-07-05 22:25:33 -0700

    Added minimal working examples.

---
+++ examples/classification_minimal_working_example.py
@@ -0,0 +1,58 @@
+
+# coding: utf-8
+
+# # Minimal Classification Example
+
+# In[1]:
+
+
+from __future__ import print_function, absolute_import, division, unicode_literals, with_statement
+from hyperopt.model_selection import GridSearch
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+
+# In[2]:
+
+
+data = load_breast_cancer()
+
+# Create test and train sets from one dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    data["data"], 
+    data["target"], 
+    test_size = 0.3, 
+    random_state = 0,
+    stratify = data["target"],
+)
+
+# Create a validation set.
+X_train, X_val, y_train, y_val = train_test_split(
+    X_train, 
+    y_train, 
+    test_size = 0.3, 
+    random_state = 0,
+    stratify = y_train,
+)
+
+# List the parameters to search across
+# List the parameters to search across
+param_grid = {
+    'C': [1, 10, 100, 120, 150], 
+    'gamma': [0.001, 0.0001], 
+    'kernel': ['rbf'],
+}
+
+# Grid-search all parameter combinations using a validation set.
+gs = GridSearch(model = SVC(random_state=0))
+gs.fit(X_train, y_train, param_grid, X_val, y_val)
+
+# Compare with default model without hyperopt
+default = SVC(random_state=0)
+default.fit(X_train, y_train)
+print('\nTest score comparison (larger is better):')
+print('Non-optimized Parameters:', round(default.score(X_test, y_test), 4))
+print('Optimized Parameters:', round(gs.score(X_test, y_test), 4))
+
---
+++ examples/regression_minimal_working_example.py
@@ -0,0 +1,54 @@
+
+# coding: utf-8
+
+# # Minimal Regression Example
+
+# In[1]:
+
+
+from __future__ import print_function, absolute_import, division, unicode_literals, with_statement
+from hyperopt.model_selection import GridSearch
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR
+
+
+# In[2]:
+
+
+data = load_boston()
+
+# Create test and train sets from one dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    data["data"], 
+    data["target"], 
+    test_size = 0.1, 
+    random_state = 0,
+)
+
+# Create a validation set.
+X_train, X_val, y_train, y_val = train_test_split(
+    X_train, 
+    y_train, 
+    test_size = 0.1, 
+    random_state = 0,
+)
+
+# List the parameters to search across
+param_grid = {
+    'C': [1, 10, 100, 120, 150], 
+    'gamma': [0.001, 0.0001], 
+    'kernel': ['rbf'],
+}
+
+# Grid-search all parameter combinations using a validation set.
+gs = GridSearch(model = SVR())
+gs.fit(X_train, y_train, param_grid, X_val, y_val)
+
+# Compare with default model without hyperopt
+default = SVR()
+default.fit(X_train, y_train)
+print('\nTest score comparison (larger is better):')
+print('Non-optimized Parameters:', round(default.score(X_test, y_test), 4))
+print('Optimized Parameters:', round(gs.score(X_test, y_test), 4))
+
