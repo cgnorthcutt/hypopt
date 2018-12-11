c991e251968000b5b7de3e269c779c9d6c2a2b25
Curtis G. Northcutt <curtis.northcutt@gmail.com>
2018-12-11 17:49:45 -0500

    v1.0.8 - param_grid now in GridSearch constructor

--- hypopt/version.py
+++ hypopt/version.py
@@ -1,1 +1,5 @@
-__version__ = '1.0.7'
+__version__ = '1.0.8'
+
+# 1.0.8 - Moved param_grid parameter from GridSearch.fit() to the constructor
+# 1.0.7 - Added scoring support (F1, accuracy, etc.). 100% testing coverage.
+# 1.0.6 (and below) - completely deprecated. Do not use.
