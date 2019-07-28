f42725fb5d266c20d4f9f02a9e8a6b5bb93cad57
Curtis G. Northcutt <curtis.northcutt@gmail.com>
2019-07-28 08:33:23 -0700

    UPDATE TO VERSION 1.0.9 - parallelization fixes. parallelization can be toggled off now

--- hypopt/version.py
+++ hypopt/version.py
@@ -1,4 +1,8 @@
-__version__ = '1.0.8'
+__version__ = '1.0.9'
+
+# 1.0.9 - MAJOR UPDATE. Previous versions are now deprecated.
+#       - Parallelization in folds now uses Daemon-free processes to avoid colluding model.fit() child daemon processes.
+#       - Parallelization can now be toggled off via GridSearch(parallelize=False)
 
 # 1.0.8 - Moved param_grid parameter from GridSearch.fit() to the constructor
 # 1.0.7 - Added scoring support (F1, accuracy, etc.). 100% testing coverage.
